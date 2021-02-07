9bd221cd89d7b52cc9d57ec10162b2f4928fca82
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 19:44:50 +0200

    added react-router, i18n, redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+import {Provider} from 'react-redux';
+import store from './store';
+import {SnackbarProvider} from 'notistack';
+import React from 'react';
 
 function App() {
-  return (
-
-  );
+    return (
+        <Router>
+            <Provider store={store}>
+                <SnackbarProvider maxSnack={3}>
+                    <Header/>
+                    <Content/>
+                    <Footer/>
+                </SnackbarProvider>
+            </Provider>
+        </Router>
+    );
 }
 
 export default App;
+
+// https://motherfuckingwebsite.com/
+// https://bestmotherfucking.website/
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './i18n/i18n'; // init i18n config
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
